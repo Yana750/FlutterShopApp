d77865ff4fada9c908e9b2555371e5586394802b
yaaanaaa <144522141+Yana750@users.noreply.github.com>
2025-09-06 01:32:19 +0700

    подключение и настройка событий MyTracker

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:check/Provider/favorite_provider.dart';
 import 'package:check/screen/nav_bar_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:mytracker_sdk/mytracker_sdk.dart';
 
@@ -8,11 +11,13 @@ void main() async {
   //неатрибутивная установка (install)
   //отслеживает установку приложения при первом запуске
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: "assets/.env");
 
-  const String sdkKey = "77024653350660193659";
+  final sdkKey = dotenv.env['MY_TRACKER_SDK'];
+  print('SDK KEY MYTRACKER $sdkKey');
 
   try {
-    await MyTracker.init(sdkKey);
+    await MyTracker.init(sdkKey!);
     print("Трекер SDK успешно подключен");
   } catch (e) {
     print("Ошибка инициализации");
